f5f4426eb1a5e9bbff8077bad8ff68f0d6e0d538
abhashsolanki <abhashsolanki18@gmail.com>
2024-05-17 16:29:00 +0530

    deploy

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,12 @@
-import express from 'express';
-import cors from 'cors';
-import multer from 'multer';
-import { uploadOnCloud } from './cloudinary.js';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import dotenv from 'dotenv';
-import axios from 'axios';
-import collection from './mongo.js';
+import express from "express";
+import cors from "cors";
+import multer from "multer";
+import { uploadOnCloud } from "./cloudinary.js";
+import path from "path";
+import { fileURLToPath } from "url";
+import dotenv from "dotenv";
+import axios from "axios";
+import collection from "./mongo.js";
 dotenv.config();
 
 const app = express();
@@ -14,71 +14,64 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
 
 
+app.use(cors(
+{
+origin: ["https://mern-assignment-frontend-rho.vercel.app/"],
+methods: ["POST","GET"],
+credentials: true
+}
+));
 
-  app.get("/",cors(),(req,res)=>{
+app.get("/", (req, res) => {
+  
+});
 
-  })
+app.post("/", async (req, res) => {
+  const { email } = req.body;
 
+  try {
+    const check = await collection.findOne({ email: email });
 
-  app.post("/",async(req,res)=>{
-      const{email}=req.body
-
-      try{
-          const check=await collection.findOne({email:email})
-
-          if(check){
-              res.json("exist")
-          }
-          else{
-              res.json("notexist")
-          }   
-
-      }
-      catch(e){
-          res.json("fail")
-      }
-
-  })
-
-
-
-app.post("/signup",async(req,res)=>{
-    const{email,name,password}=req.body
-
-    const data={
-        email:email,
-        name:name,
-        password:password
+    if (check) {
+      res.json("exist");
+    } else {
+      res.json("notexist");
     }
+  } catch (e) {
+    res.json("fail");
+  }
+});
 
-    try{
-        const check=await collection.findOne({email:email})
+app.post("/signup", async (req, res) => {
+  const { email, name, password } = req.body;
 
-        if(check){
-            res.json("exist")
-        }
-        else{
-            res.json("notexist")
-            await collection.insertMany([data])
-        }
+  const data = {
+    email: email,
+    name: name,
+    password: password,
+  };
 
+  try {
+    const check = await collection.findOne({ email: email });
+
+    if (check) {
+      res.json("exist");
+    } else {
+      res.json("notexist");
+      await collection.insertMany([data]);
     }
-    catch(e){
-        res.json("fail")
-    }
-
-})
-
+  } catch (e) {
+    res.json("fail");
+  }
+});
 
 const imageViewCounts = {};
 
-
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './public/temp');
+    cb(null, "./public/temp");
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now();
@@ -88,34 +81,40 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-app.post('/upload', upload.single('image'), async (req, res) => {
+app.post("/upload", upload.single("image"), async (req, res) => {
   const { title, description } = req.body;
   const localFilePath = req.file.path;
 
   try {
     const imageUrl = await uploadOnCloud(localFilePath, title, description);
-    res.status(200).json({ message: 'File uploaded successfully', url: imageUrl });
+    res
+      .status(200)
+      .json({ message: "File uploaded successfully", url: imageUrl });
   } catch (error) {
-    res.status(500).json({ message: 'File upload failed', error: error.message });
+    res
+      .status(500)
+      .json({ message: "File upload failed", error: error.message });
   }
 });
 
-app.get('/images', async (req, res) => {
+app.get("/images", async (req, res) => {
   try {
-    const cloudName = 'abhashsolanki'; 
-    const folder = 'images'; 
+    const cloudName = "abhashsolanki";
+    const folder = "images";
 
     const response = await axios.get(
       `https://api.cloudinary.com/v1_1/${cloudName}/resources/search`,
       {
         params: {
           expression: `folder:${folder}`,
-          type: 'upload', 
+          type: "upload",
         },
         headers: {
           Authorization: `Basic ${Buffer.from(
-            process.env.CLOUDINARY_API_KEY + ':' + process.env.CLOUDINARY_API_SECRET
-          ).toString('base64')}`,
+            process.env.CLOUDINARY_API_KEY +
+              ":" +
+              process.env.CLOUDINARY_API_SECRET
+          ).toString("base64")}`,
         },
       }
     );
@@ -124,22 +123,26 @@ app.get('/images', async (req, res) => {
       // Increment view count for each image
       const imagesWithViews = response.data.resources.map((image) => {
         const publicId = image.public_id;
-        imageViewCounts[publicId] = imageViewCounts[publicId] ? imageViewCounts[publicId] + 1 : 1;
+        imageViewCounts[publicId] = imageViewCounts[publicId]
+          ? imageViewCounts[publicId] + 1
+          : 1;
         return {
           public_id: publicId,
           secure_url: image.secure_url,
           views: imageViewCounts[publicId], // Attach view count to each image
-          description: image.context ? image.context.custom.description : '',
+          description: image.context ? image.context.custom.description : "",
         };
       });
 
       res.status(200).json(imagesWithViews);
     } else {
-      res.status(404).json({ message: 'No images found in the specified folder' });
+      res
+        .status(404)
+        .json({ message: "No images found in the specified folder" });
     }
   } catch (error) {
-    console.error('Error fetching images:', error);
-    res.status(500).json({ message: 'Internal server error' });
+    console.error("Error fetching images:", error);
+    res.status(500).json({ message: "Internal server error" });
   }
 });
 
